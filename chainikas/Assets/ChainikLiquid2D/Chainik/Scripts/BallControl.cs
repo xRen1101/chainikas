e5d5f448e29ecac8d60ca6d450505fdd6e6827b7
serpaulius <paulius.sereiva@gmail.com>
2017-01-22 13:03:52 +0200

    Justas commit (many changes)

--- chainikas/Assets/ChainikLiquid2D/Chainik/Scripts/BallControl.cs
+++ chainikas/Assets/ChainikLiquid2D/Chainik/Scripts/BallControl.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour {
 
+    public GameObject air;
+
+    private bool movingUp = false;
+    private Rigidbody2D rig;
+    private float gravityScale = 0f;
+    bool dropping = false;
+    bool airAdded = false;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        rig = GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
+        if ((transform.position.y >= 0.1f) && movingUp)
         {
-            GetComponent<Rigidbody2D>().gravityScale = 1;
+            rig.gravityScale = 0;
+            //rig.inertia = 0; //FunStuff, things will break
+            rig.velocity = Vector2.zero;
+            transform.position = new Vector3 (transform.position.x ,0.1f, transform.position.z);
+            movingUp = false;
         }
-	}
+        if (Input.GetButtonDown("DropIt"))
+        {
+            rig.gravityScale = 1;
+            dropping = true;
+        }
+        if (Input.GetButtonUp("DropIt"))
+        {
+            rig.gravityScale = -1;
+            movingUp = true;
+            dropping = false;
+        }
+        if (dropping)
+        {
+            rig.gravityScale += 0.1f;
+        }
+        if (transform.position.y <= -3.5f && !airAdded)
+        {
+            airAdded = true;
+            air.transform.position += new Vector3(0, -0.05f, 0);
+        }
+        if (transform.position.y >= -3.5f)
+            airAdded = false;
+    }
 }
